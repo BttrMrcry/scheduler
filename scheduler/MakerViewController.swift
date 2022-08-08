da246a6248e1706b8128ba960be81a1789a0f032
Ricardo López <bettermercury@comunidad.unam.mx>
2022-08-08 11:47:29 -0500

    Makes MakerViewController able to show a table view

--- scheduler/MakerViewController.swift
+++ scheduler/MakerViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class MakerViewController: UIViewController {
-
+class MakerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    // Return the number of rows for the table.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return 20
     }
 
+    // Provide a cell object for each row.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       // Fetch a cell of the appropriate type.
+       let cell = tableView.dequeueReusableCell(withIdentifier: "makerCell", for: indexPath)
+       
+       // Configure the cell’s contents.
+       cell.textLabel!.text = "Cell text"
+           
+       return cell
+    }
+    
+    
 
 }
 
