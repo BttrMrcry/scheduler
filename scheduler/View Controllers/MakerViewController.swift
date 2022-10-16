ba5668a92e6b8bcc5e9571c098608543c9a9b8da
Diego Nunez <90226979+xDiegoNunezx@users.noreply.github.com>
2022-10-15 23:58:41 -0500

    c1

--- scheduler/View Controllers/MakerViewController.swift
+++ scheduler/View Controllers/MakerViewController.swift
@@ -1,38 +1,21 @@
-//
-//  ViewController.swift
-//  scheduler
-//
-//  Created by rl on 04/08/22.
-//
-
 import UIKit
-
-
-
 
 class MakerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var editButton: UIBarButtonItem!
     var subjectsController:SubjectsController = SubjectsController()
-    @IBAction func EditButtonTapped(_ sender: UIBarButtonItem) {
-        let tableViewEditMode = tableView.isEditing
-        if tableViewEditMode {
-            editButton.title = "Editar"
-        }else{
-            editButton.title = "Listo"
-        }
-        
-        tableView.setEditing(!tableViewEditMode, animated: true)
-    }
+    var subjects: [Subject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.leftBarButtonItem = editButtonItem
         tableView.delegate = self
         tableView.dataSource = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         subjectsController.loadSubjects()
+        subjects = subjectsController.subjects
+        tableView.reloadData()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -42,7 +25,7 @@ class MakerViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     // Return the number of rows for the table.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return subjectsController.subjects.count
+        return subjects.count
     }
 
     // Provide a cell object for each row.
@@ -51,25 +34,19 @@ class MakerViewController: UIViewController, UITableViewDelegate, UITableViewDat
         // Fetch a cell of the appropriate type.
         let cell = tableView.dequeueReusableCell(withIdentifier: "makerCell", for: indexPath)
        // Configure the cell’s contents.
-        cell.textLabel!.text = subjectsController.subjects[indexPath.row].name
+        cell.textLabel!.text = subjects[indexPath.row].name
         
-        if tableView.isEditing {
-            cell.showsReorderControl = true
-        }else{
-            cell.showsReorderControl = false
-        }
         return cell
-        
     }
     
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        let subjectToMove = subjectsController.subjects.remove(at: sourceIndexPath.row)
-        subjectsController.subjects.insert(subjectToMove, at: destinationIndexPath.row)
+        let subjectToMove = subjects.remove(at: sourceIndexPath.row)
+        subjects.insert(subjectToMove, at: destinationIndexPath.row)
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            subjectsController.subjects.remove(at: indexPath.row)
+            subjects.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .none)
         }
     }
@@ -86,6 +63,5 @@ class MakerViewController: UIViewController, UITableViewDelegate, UITableViewDat
         }
     }
     
-
 }
 
